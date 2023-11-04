cb81b55d26dbcd2b0792ffb9ae63a6cbf8edc323
rabie <r.raad@xpay.de>
2023-11-04 14:59:39 +0100

    feat: implement auth

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+
 
 
 
@@ -115,4 +117,18 @@ export async function updateInvoice(
 export async function deleteInvoice(id: string) {
       throw new Error('Failed to Delete Invoice');
 
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', Object.fromEntries(formData));
+  } catch (error) {
+    if ((error as Error).message.includes('CredentialsSignin')) {
+      return 'CredentialSignin';
+    }
+    throw error;
+  }
 }
